ab6dc9c64eec22178798381ab55a391487bc99ea
François Galea <fgalea@free.fr>
2020-09-14 20:12:32 +0200

    Updated the IKBD testbench.

--- sim/ikbd_tb.vhd
+++ sim/ikbd_tb.vhd
@@ -25,6 +25,8 @@ end ikbd_tb;
 architecture dut of ikbd_tb is
 
 	signal clk			: std_logic := '0';
+	signal clkren		: std_logic := '0';
+	signal clkfen		: std_logic := '0';
 	signal reset		: std_logic;
 	signal rx			: std_logic := '1';
 	signal tx			: std_logic;
@@ -36,6 +38,8 @@ architecture dut of ikbd_tb is
 	component atari_ikbd is
 		port (
 			clk		: in std_logic;
+			clkren	: in std_logic;
+			clkfen	: in std_logic;
 			reset	: in std_logic;
 			rx		: in std_logic;
 			tx		: out std_logic;
@@ -45,10 +49,32 @@ architecture dut of ikbd_tb is
 		);
 	end component;
 
+	component clock_enabler is
+		port (
+			clk			: in std_logic;
+			reset		: in std_logic;
+			enNC1		: in std_logic;		-- enable 8 MHz rising edges
+			enNC2		: in std_logic;		-- enable 8 MHz falling edges
+			en8rck		: out std_logic;	-- 8 MHz rising edge
+			en8fck		: out std_logic;	-- 8 MHz falling edge
+			en32ck		: out std_logic;	-- 32 MHz rising edge
+			en4rck		: out std_logic;	-- 4 MHz rising edge
+			en4fck		: out std_logic;	-- 4 MHz falling edge
+			en2rck		: out std_logic;	-- 2 MHz rising edge
+			en2fck		: out std_logic;	-- 2 MHz falling edge
+			en2_4576	: out std_logic;	-- 2.4576 MHz rising edge
+			ck05		: out std_logic;	-- 500 kHz clock
+			error		: out std_logic		-- time out error
+		);
+	end component;
+
+
 begin
 
 	ikbd:atari_ikbd port map (
 		clk => clk,
+		clkren => clkren,
+		clkfen => clkfen,
 		reset => reset,
 		rx => rx,
 		tx => tx,
@@ -57,9 +83,18 @@ begin
 		k => k
 	);
 
-	clk <= not clk after 125 ns;		-- 4 MHz
-	reset <= '1', '0' after 2442 ns;
-	rx <= '1', '0' after 30 us, '1' after 500 us;
-	k(2) <= '1', '0' after 8 us, '1' after 820 us;
+	clken:clock_enabler port map (
+		clk => clk,
+		reset => reset,
+		enNC1 => '1',
+		enNC2 => '1',
+		en2rck => clkren,
+		en2fck => clkfen
+	);
+
+	clk <= not clk after 5 ns;		-- 100 MHz
+	reset <= '0', '1' after 300 us, '0' after 510 us;
+	rx <= '1', '0' after 600 us, '1' after 220 ms;	-- processor reset
+	k(91) <= '1', '0' after 350 ms, '1' after 500 ms; 	-- [/] key, scan code 0x35
 
 end dut;
