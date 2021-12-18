7b6085310d7c237cb7ac22adb055497514e5328f
François Galea <fgalea@free.fr>
2021-12-18 09:42:16 +0100

    Auto-generated modules as components.

--- hdl/zest_z7lite_top.vhd
+++ hdl/zest_z7lite_top.vhd
@@ -56,6 +56,57 @@ end zest_top;
 
 
 architecture structure of zest_top is
+	component ps_domain is
+		port (
+			A_0 : in std_logic_vector(31 downto 0);
+			DDR_addr : inout std_logic_vector(14 downto 0);
+			DDR_ba : inout std_logic_vector(2 downto 0);
+			DDR_cas_n : inout std_logic;
+			DDR_ck_n : inout std_logic;
+			DDR_ck_p : inout std_logic;
+			DDR_cke : inout std_logic;
+			DDR_cs_n : inout std_logic;
+			DDR_dm : inout std_logic_vector(3 downto 0);
+			DDR_dq : inout std_logic_vector(31 downto 0);
+			DDR_dqs_n : inout std_logic_vector(3 downto 0);
+			DDR_dqs_p : inout std_logic_vector(3 downto 0);
+			DDR_odt : inout std_logic;
+			DDR_ras_n : inout std_logic;
+			DDR_reset_n : inout std_logic;
+			DDR_we_n : inout std_logic;
+			DS_0 : in std_logic_vector(1 downto 0);
+			ERROR_0 : out std_logic;
+			FIXED_IO_ddr_vrn : inout std_logic;
+			FIXED_IO_ddr_vrp : inout std_logic;
+			FIXED_IO_mio : inout std_logic_vector(53 downto 0);
+			FIXED_IO_ps_clk : inout std_logic;
+			FIXED_IO_ps_porb : inout std_logic;
+			FIXED_IO_ps_srstb : inout std_logic;
+			IRQ_F2P_0 : in std_logic_vector(0 to 0);
+			OFFSET_0 : in std_logic_vector(31 downto 0);
+			OFFVALD_0 : in std_logic;
+			R_0 : in std_logic;
+			R_DONE_0 : out std_logic;
+			W_0 : in std_logic;
+			W_DONE_0 : out std_logic;
+			clk : out std_logic;
+			iD_0 : in std_logic_vector(15 downto 0);
+			in_reg0_0 : in std_logic_vector(31 downto 0);
+			in_reg1_0 : in std_logic_vector(31 downto 0);
+			oD_0 : out std_logic_vector(15 downto 0);
+			out_reg0_0 : out std_logic_vector(31 downto 0);
+			out_reg1_0 : out std_logic_vector(31 downto 0);
+			out_reg2_0 : out std_logic_vector(31 downto 0);
+			out_reg3_0 : out std_logic_vector(31 downto 0);
+			out_reg4_0 : out std_logic_vector(31 downto 0);
+			out_reg5_0 : out std_logic_vector(31 downto 0);
+			out_reg6_0 : out std_logic_vector(31 downto 0);
+			out_reg7_0 : out std_logic_vector(31 downto 0);
+			resetn : out std_logic;
+			vid5_clk : out std_logic;
+			vid_clk : out std_logic
+		);
+	end component;
 
 	signal clk			: std_logic;
 	signal resetn		: std_logic;
@@ -152,7 +203,7 @@ begin
 	ram_offset <= out_reg1;
 	in_reg0(10 downto 0) <= (others => '0');
 
-	psd:entity ps_domain port map(
+	psd:ps_domain port map(
 		DDR_addr => DDR_addr,
 		DDR_ba => DDR_ba,
 		DDR_cas_n => DDR_cas_n,
--- hdl/zest_zturn_top.vhd
+++ hdl/zest_zturn_top.vhd
@@ -62,6 +62,59 @@ end zest_top;
 
 
 architecture structure of zest_top is
+	component ps_domain_wrapper is
+		port (
+			A_0 : in std_logic_vector(31 downto 0);
+			DDR_addr : inout std_logic_vector(14 downto 0);
+			DDR_ba : inout std_logic_vector(2 downto 0);
+			DDR_cas_n : inout std_logic;
+			DDR_ck_n : inout std_logic;
+			DDR_ck_p : inout std_logic;
+			DDR_cke : inout std_logic;
+			DDR_cs_n : inout std_logic;
+			DDR_dm : inout std_logic_vector(3 downto 0);
+			DDR_dq : inout std_logic_vector(31 downto 0);
+			DDR_dqs_n : inout std_logic_vector(3 downto 0);
+			DDR_dqs_p : inout std_logic_vector(3 downto 0);
+			DDR_odt : inout std_logic;
+			DDR_ras_n : inout std_logic;
+			DDR_reset_n : inout std_logic;
+			DDR_we_n : inout std_logic;
+			DS_0 : in std_logic_vector(1 downto 0);
+			ERROR_0 : out std_logic;
+			FIXED_IO_ddr_vrn : inout std_logic;
+			FIXED_IO_ddr_vrp : inout std_logic;
+			FIXED_IO_mio : inout std_logic_vector(53 downto 0);
+			FIXED_IO_ps_clk : inout std_logic;
+			FIXED_IO_ps_porb : inout std_logic;
+			FIXED_IO_ps_srstb : inout std_logic;
+			IIC_0_0_scl_io : inout std_logic;
+			IIC_0_0_sda_io : inout std_logic;
+			IRQ_F2P_0 : in std_logic_vector(0 to 0);
+			OFFSET_0 : in std_logic_vector(31 downto 0);
+			OFFVALD_0 : in std_logic;
+			R_0 : in std_logic;
+			R_DONE_0 : out std_logic;
+			W_0 : in std_logic;
+			W_DONE_0 : out std_logic;
+			clk : out std_logic;
+			iD_0 : in std_logic_vector(15 downto 0);
+			in_reg0_0 : in std_logic_vector(31 downto 0);
+			in_reg1_0 : in std_logic_vector(31 downto 0);
+			oD_0 : out std_logic_vector(15 downto 0);
+			out_reg0_0 : out std_logic_vector(31 downto 0);
+			out_reg1_0 : out std_logic_vector(31 downto 0);
+			out_reg2_0 : out std_logic_vector(31 downto 0);
+			out_reg3_0 : out std_logic_vector(31 downto 0);
+			out_reg4_0 : out std_logic_vector(31 downto 0);
+			out_reg5_0 : out std_logic_vector(31 downto 0);
+			out_reg6_0 : out std_logic_vector(31 downto 0);
+			out_reg7_0 : out std_logic_vector(31 downto 0);
+			resetn : out std_logic;
+			vid_clk : out std_logic
+		);
+	end component;
+
 	signal clk			: std_logic;
 	signal resetn		: std_logic;
 	signal pclk			: std_logic;
@@ -154,7 +207,7 @@ begin
 	ram_offset <= out_reg1;
 	in_reg0(10 downto 0) <= (others => '0');
 
-	psd:entity ps_domain_wrapper port map(
+	psd:ps_domain_wrapper port map(
 		DDR_addr => DDR_addr,
 		DDR_ba => DDR_ba,
 		DDR_cas_n => DDR_cas_n,
